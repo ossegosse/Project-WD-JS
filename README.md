c50507a2cedefbc37e289040a49ff68fbc7ee06d
ossegosse <ossian.igelberg@gmail.com>
2023-10-26 15:32:53 +0200

    First commit Main

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Project-WD-JS
 Examinationsuppgift WD-JS
+
+En webbplats byggd med HTML, CSS & vanilla JS om vårt solsystem. Det ska gå att klicka på en planet och där få mer information om planeten i någon form av "overlay". Se skiss nedan.
+
---
+++ script.js
@@ -0,0 +1,13 @@
+
+// - Skapa sida som visar solsystemet enligt skiss
+// - Använda ZoComs API som innehåller info om planeterna
+// - Få tag i API genom att använda deras Key för GET request
+// - Planeterna ska ha (egen sida, ny html?) beskrivning hämtad från API
+// - API-informationen ska renderas ut i DOM
+// - Kunna interagera med sidan genom att klicka på planeterna
+// - Kunna återvända till startsida (hela solsystemet)
+
+
+
+const url = "https://fathomless-shelf-54969.herokuapp.com/"
+
